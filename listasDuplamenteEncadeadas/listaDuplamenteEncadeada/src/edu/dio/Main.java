27569c9770d58ed935ad823b1666057cea7e0774
rbcerezer <rb.cerezer@gmail.com>
2023-03-16 23:02:46 +0100

    Atualizando o projeto de Listas Duplamente Encadeadas com o método toString e a criação da Classe Main.

---
+++ listasDuplamenteEncadeadas/listaDuplamenteEncadeada/src/edu/dio/Main.java
@@ -0,0 +1,24 @@
+package edu.dio;
+
+public class Main {
+    public static void main(String[] args) {
+
+        listaDuplamenteEncadeada<String> minhaListaEncadeada = new listaDuplamenteEncadeada<>();
+
+        minhaListaEncadeada.add("c1");
+        minhaListaEncadeada.add("c2");
+        minhaListaEncadeada.add("c3");
+        minhaListaEncadeada.add("c4");
+        minhaListaEncadeada.add("c5");
+        minhaListaEncadeada.add("c6");
+        minhaListaEncadeada.add("c7");
+
+        System.out.println(minhaListaEncadeada);
+
+        minhaListaEncadeada.remove(3);
+        minhaListaEncadeada.add(3, "99");
+
+        System.out.println(minhaListaEncadeada);
+        System.out.println(minhaListaEncadeada.get(3));
+    }
+}
